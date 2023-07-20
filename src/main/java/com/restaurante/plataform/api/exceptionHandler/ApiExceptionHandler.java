87273656b36998f4898f4fc26e7860703fce4a0f
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-07-20 02:24:42 -0300

    Tratado handleHttpMessageNotReadable

--- src/main/java/com/restaurante/plataform/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/restaurante/plataform/api/exceptionHandler/ApiExceptionHandler.java
@@ -4,12 +4,14 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -20,6 +22,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.restaurante.plataform.domain.exception.EntityNotFoundException;
 import com.restaurante.plataform.domain.exception.ExceptionBusiness;
 import com.restaurante.plataform.domain.exception.UseEntityException;
@@ -49,11 +54,35 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 	
 	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+	
+		Throwable rootCause = ExceptionUtils.getRootCause(ex);
+		
+		if (rootCause instanceof InvalidFormatException) {
+			return handleInvalidFormat((InvalidFormatException) rootCause, headers, status, request);
+		}else if (rootCause instanceof PropertyBindingException) {
+			return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request);
+		}
+		
+		ProblemType problemType = ProblemType.MESSAGE_INCOMPREHENSIBLE;
+		
+		String detail = " O corpo da requisição está inválido. Verifique erro de sintaxe";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(detail)
+				.build();
+		
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+	
+	@Override
 	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		ProblemType problemType = ProblemType.METHOD_NOT_SUPORTED;
-		String detail = "Entidade não pode ser encontrada para atualização";
+		String detail = "Entidade não pode ser encontrada para atualização, preencha corretamente o código da entidade";
 		
 		Problem problem = createProblemBuilder(status, problemType, detail)
 				.userMessage(GENRIC_MSG_FINAL_USER)
@@ -154,6 +183,41 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 	
 	
+	private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request){
+		
+		String path = joinPath(ex.getPath());
+		
+		ProblemType problemType = ProblemType.MESSAGE_INCOMPREHENSIBLE;
+		String detail = String.format("A propriedade '%s' recebeu o valor '%s',  "
+				+ "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo '%s'. ",
+				path, ex.getValue(), ex.getTargetType().getSimpleName());
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(GENRIC_MSG_FINAL_USER)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+		
+	}
+	
+	private ResponseEntity<Object> handlePropertyBinding(PropertyBindingException e,
+			HttpHeaders headers, HttpStatus status, WebRequest request){
+		
+		String path = joinPath(e.getPath());
+		
+		ProblemType problemType = ProblemType.MESSAGE_INCOMPREHENSIBLE;
+		String detail = String.format("A propriedade '%s' não existe. "
+	            + "Corrija ou remova essa propriedade e tente novamente.", path);
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(GENRIC_MSG_FINAL_USER)
+				.build();
+		
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+	}
+	
+	
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
@@ -188,5 +252,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.type(problemType.getUri())
 				.title(problemType.getTitle())
 				.detail(detail);
+	}
+	
+	private String joinPath(List<Reference> references) {
+		return references.stream()
+				.map( ref -> ref.getFieldName())
+				.collect(Collectors.joining("."));
+
 	}
 }
--- src/main/java/com/restaurante/plataform/api/model/input/UserInput.java
+++ src/main/java/com/restaurante/plataform/api/model/input/UserInput.java
@@ -3,6 +3,7 @@ package com.restaurante.plataform.api.model.input;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Getter;
@@ -16,6 +17,7 @@ public class UserInput {
 	@NotBlank
 	private String celphone;
 	@NotBlank
+	@Email
 	private String email;
 	
 	private List<TablesInput> responsibleForTables = new ArrayList<>(); 
