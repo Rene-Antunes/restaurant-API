28cb67334ccf9819f1f7555df5eed7fb349f7d8f
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-07-21 21:51:57 -0300

    Adicionado tabela PhotoProduct

--- src/main/java/com/restaurante/plataform/domain/model/PhotoProduct.java
+++ src/main/java/com/restaurante/plataform/domain/model/PhotoProduct.java
@@ -1,11 +1,30 @@
 package com.restaurante.plataform.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode
+@Entity
 public class PhotoProduct {
 	
-	private String name;
+	@EqualsAndHashCode.Include
+	@Id
+	@Column
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	private Product product;
+	
+	private String fileName;
 	private String description;
 	private String contentType;
 	private Long size;
